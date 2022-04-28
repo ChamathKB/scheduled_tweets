fd79f6a950b6cb2ea8d7fb4da223f1022b09ab3b
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2022-04-28 21:03:00 +0530

    added user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   # get "/", to: "main#index"
   root to: "main#index"
   # Defines the root path route ("/")
