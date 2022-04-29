c02dcc68c742ea619ab4296468c86d888c34a68f
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2022-04-29 17:13:12 +0530

    added user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,21 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index"
 
-  get "sign_up", to: "registrations#new"
+  get "password", to: "passwords#edit"
+  post "password", to: "passwords#update"
+
+  get "password/reset", to: "password_rests#new"
+  post "password/reset", to: "password_rests#create"
+
+  get "sign_up", to: "registrations#new", as: :edit_password
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
+
+  delete "logout", to: "sessions#destroy"
 
   # get "/", to: "main#index"
   root to: "main#index"
