da4d2bf6da08726ec0c7f35c42fb407ac2a244f8
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2022-04-29 17:56:21 +0530

    added twitter connection

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   get "password/reset/edit", to: "password_resets#edit"
   patch "password/reset/edit", to: "password_resets#update"
 
+  get "/auth/twitter/callback", to: "omniauth_callbacks#twitter"
   delete "logout", to: "sessions#destroy"
 
   # get "/", to: "main#index"
